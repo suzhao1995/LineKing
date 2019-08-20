25d6b98426a14d9640b02e4bceb0c9ef94d14368
suzhao <408706083@qq.com>
2019-08-20 17:03:56 +0800

    修改发送消息时间

--- teach-project/teach-web/src/main/java/com/rs/teach/controller/index/IndexController.java
+++ teach-project/teach-web/src/main/java/com/rs/teach/controller/index/IndexController.java
@@ -124,7 +124,7 @@ public class IndexController {
 				if(Integer.valueOf(today)+7 > Integer.valueOf(endDate)){
 					Message message = new Message();
 					message.setMessageContent("您的系统使用权限即将到期！");
-					message.setMessageTime(DateUtil.dateFormat(new Date(), "yyyy-MM-dd hh:mm:ss"));
+					message.setMessageTime(DateUtil.dateFormat(new Date(), "yyyy-MM-dd HH:mm:ss"));
 					message.setMessageType("2");
 					message.setUserId(loginUser.getUserId());
 					messageService.addMessage(message);
