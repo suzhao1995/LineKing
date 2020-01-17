123b9a8c85e4a1005e2aa14f51a091c43da6aefc
alice97hy <alice97hy@163.com>
2020-01-17 14:09:52 +0800

    提示语修改

--- teach-project/teach-web/src/main/java/com/rs/teach/controller/backstage/BeforeVideoController.java
+++ teach-project/teach-web/src/main/java/com/rs/teach/controller/backstage/BeforeVideoController.java
@@ -239,6 +239,9 @@ public class BeforeVideoController {
                 DeleteFileUtil.deleteFile(video.getVideoPath());
                 bean.addError(ResponseBean.CODE_MESSAGE_ERROR, "上传失败");
             }
+        }else{
+            logger.error("错误信息"+resultMap.get("message"));
+            bean.addError((String) resultMap.get("message"));
         }
 
         return bean;
